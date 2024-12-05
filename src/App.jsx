8ab3d44f2dd924033547c6d7e4e7fa5771a31e7f
Sumit00s <sonawanesumit245@gmail.com>
2024-12-05 22:41:39 +0530

    User Authentication using clerk

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ const router = createBrowserRouter([
         element:<PostJobs/>
       },
       {
-        path:'/saved-job',
+        path:'/saved-jobs',
         element:<SavedJobs/>
       },
       {
